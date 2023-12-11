996a902b98962e609d4a95bc245d1ba992592a4e
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-11 21:26:28 +0100

    add about

---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section id="about" className="container mx-auto flex flex-col items-center">
+      <div className="block lg:max-w-xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-2xl">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Hey!</h5>
+        <p className="font-sans text-gray-70">
+          Mein Name ist Vanda. Ich bin promovierter Mathematiker und erfahrener Python-Entwickler. Gerne helfe ich dir
+          bei der Konzeption, Erstellung und Optimierung deiner Webanwendung.
+        </p>
+      </div>
+    </section>
+  );
+}
