d9d3971ae156e0b3deb0cc30fa5cc67fd78ea62b
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-12 11:34:03 +0100

    transform to arrow function component

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,13 +1,17 @@
-export default function About() {
+const About = () => {
   return (
-    <section id="about" className="container mx-auto flex flex-col items-center">
-      <div className="block lg:max-w-xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-2xl">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Hey!</h5>
-        <p className="font-sans text-gray-70">
-          Mein Name ist Vanda. Ich bin promovierter Mathematiker und erfahrener Python-Entwickler. Gerne helfe ich dir
-          bei der Konzeption, Erstellung und Optimierung deiner Webanwendung.
-        </p>
+    <section id="about" className="py-20 bg-slate-700">
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="block lg:max-w-xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-2xl">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Hey!</h5>
+          <p className="font-sans text-gray-70">
+            Mein Name ist Vanda. Ich bin promovierter Mathematiker und erfahrener Python-Entwickler. Gerne helfe ich dir
+            bei der Konzeption, Erstellung und Optimierung deiner Webanwendung.
+          </p>
+        </div>
       </div>
     </section>
   );
-}
+};
+
+export default About;
