9437dcae1e3e472c68180d47472a2b0e87ac74ce
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-12 21:04:55 +0100

    add portrait in about section

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,14 +1,17 @@
+import photo from "../assets/foto.jpg";
+
 const About = () => {
   return (
-    <section id="about" className="py-20 bg-slate-700">
-      <div className="container mx-auto flex flex-col items-center">
-        <div className="block lg:max-w-xl max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-2xl">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Hey!</h5>
-          <p className="font-sans text-gray-70">
+    <section id="about" className="w-full h-screen font-sans bg-slate-500 flex flex-col justify-center items-center">
+      <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+        <img className="rounded-2xl shadow-sm shadow-slate-400 max-w-xs mr-4" src={photo} alt="portrait" />
+        <article className="p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
+          <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Hey!</h2>
+          <p>
             Mein Name ist Vanda. Ich bin promovierter Mathematiker und erfahrener Python-Entwickler. Gerne helfe ich dir
             bei der Konzeption, Erstellung und Optimierung deiner Webanwendung.
           </p>
-        </div>
+        </article>
       </div>
     </section>
   );
