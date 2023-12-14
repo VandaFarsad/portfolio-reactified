d6c36195baa1f329f5a207c4c1d02be7c12898af
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-14 10:05:17 +0100

    add sm breakpoints

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,9 +3,9 @@ import manAtWork from "../assets/man_at_work.svg";
 
 const Header = () => {
   return (
-    <div className="w-full h-screen font-mono container mx-auto flex px-10 py-20 flex-col items-center">
-      <img className="max-w-sm md:max-w-lg" alt="hero" src={manAtWork} />
-      <h1 className="font-semibold pt-2 md:pt-8 text-xl md:text-2xl ">
+    <div className="w-full sm:h-screen font-mono container mx-auto flex px-4 sm:px-10 py-10 sm:py-20 flex-col items-center">
+      <img className="w-3/4 sm:max-w-sm md:max-w-lg" alt="hero" src={manAtWork} />
+      <h1 className="font-semibold pt-2 md:pt-8 text-lg sm:text-xl md:text-2xl ">
         <TypeAnimation sequence={["Dr. Vanda Farsad"]} wrapper="span" speed={30} repeat={0} cursor={false} />
       </h1>
       <div>
