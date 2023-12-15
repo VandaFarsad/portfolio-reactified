32550521554357004adcedb531a5cabf05ce649f
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-15 11:40:34 +0100

    adapt iconiy tailwind and react interaction

--- frontend/src/components/Stack.tsx
+++ frontend/src/components/Stack.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react";
 import { iconData } from "../api/iconData";
 
 const Stack = () => {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setIsSmallScreen(window.innerWidth < 641);
-    });
-  }, []);
+  const iconDataTailwind = iconData.map((item) => ({
+    ...item,
+    icon: "lg:h-20 lg:w-20 md:h-14 md:w-14 h-10 w-10 icon-[" + item.iconSetPrefix + "--" + item.iconName + "]",
+  }));
 
   return (
     <section id="stack" className="w-full p-4 sm:p-10 md:p-40 font-sans flex flex-col justify-center items-center">
@@ -18,13 +14,9 @@ const Stack = () => {
         </div>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 sm:gap-8 px-4">
-        {iconData.map((data, index) => (
+        {iconDataTailwind.map((data, index) => (
           <div key={index} className="flex flex-col justify-center items-center px-4 sm:px-8">
-            <span
-              className={
-                "lg:h-20 lg:w-20 md:h-14 md:w-14 h-10 w-10 icon-[" + data.iconSetPrefix + "--" + data.iconName + "]"
-              }
-            ></span>
+            <span className={data.icon}></span>
             <p className="text-sm sm:text-base">{data.text}</p>
           </div>
         ))}
