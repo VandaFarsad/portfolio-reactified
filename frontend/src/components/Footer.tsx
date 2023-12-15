d35f6e80e0292fd75558fe6d5fd8b4c5c2bcccc8
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-15 14:11:46 +0100

    increase icon size for smaller screens

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,16 +3,16 @@ const Footer = () => {
     <footer className="container mx-auto px-10 py-4 flex flex-col items-center">
       <div className="flex space-x-4 mt-8 items-center">
         <a href="https://github.com/VandaFarsad" target="_blank" rel="noopener noreferrer">
-          <span className={"lg:h-7 lg:w-7 h-5 w-5 icon-[mdi--github]"}></span>
+          <span className={"h-7 w-7 icon-[mdi--github]"}></span>
         </a>
         <a href="https://www.linkedin.com/in/vanda-farsad-4b98321a5/" target="_blank" rel="noopener noreferrer">
-          <span className={"lg:h-7 lg:w-7 h-5 w-5 icon-[mdi--linkedin]"}></span>
+          <span className={"h-7 w-7 icon-[mdi--linkedin]"}></span>
         </a>
         <a href="https://www.xing.com/profile/Vanda_Farsad" target="_blank" rel="noopener noreferrer">
-          <span className={"lg:h-7 lg:w-7 h-5 w-5 icon-[mdi--xing]"}></span>
+          <span className={"h-7 w-7 icon-[mdi--xing]"}></span>
         </a>
         <a href="mailto:contact@initial-commit.com">
-          <span className={"lg:h-7 lg:w-7 h-5 w-5 icon-[mdi--email]"}></span>
+          <span className={"h-7 w-7 icon-[mdi--email]"}></span>
         </a>
       </div>
 
