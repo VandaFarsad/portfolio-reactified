d36dfb4a12defd01adf59f55e4661f24675c6a2b
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-15 09:29:06 +0100

    optimize margin

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -9,17 +9,18 @@ const About = () => {
       <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-pink-600">About</p>
       <div className="w-full pt-8 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-16">
         <img
-          className="rounded-2xl shadow-2xl shadow-slate-300 max-w-xs sm:max-w-sm sm:max-h-xl mx-auto sm:mr-4"
+          className="rounded-2xl shadow-2xl shadow-slate-300 max-w-xs mx-auto sm:max-w-sm sm:max-h-xl lg:mr-4"
           src={photo}
           alt="portrait"
         />
-        <article className="p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
+        <article className="max-w-xs sm:max-w-sm mx-auto lg:max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
           <h2 className="mb-4 text-xl sm:text-2xl font-bold tracking-tight text-gray-900">Hey!</h2>
           <p>
             Mein Name ist Vanda. Mit meinen Python-Kenntnissen und meinem Wissen über DevOps und Frontend-Technologien
             freue ich mich darauf, dich bei der Konzeption, Entwicklung und Optimierung deiner Webanwendungen zu
-            unterstützen. Lass uns gemeinsam neue und effiziente Lösungen entwickeln, die den Zielen deines Unternehmens
-            entsprechen.
+            unterstützen.
+            <br /> <br />
+            Lass uns gemeinsam neue und effiziente Lösungen entwickeln, die den Zielen deines Unternehmens entsprechen.
           </p>
         </article>
       </div>
