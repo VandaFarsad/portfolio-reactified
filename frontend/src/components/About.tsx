d887a3f0d89a505548af27dcc8f5c6a21dfcc34a
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-16 14:34:59 +0100

    left align text field

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -13,15 +13,15 @@ const About = () => {
           src={photo}
           alt="portrait"
         />
-        <article className="max-w-xs sm:max-w-sm mx-auto lg:max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
+        <article className="max-w-xs mx-auto sm:max-w-sm sm:max-h-xl lg:max-w-xl lg:ml-4 p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
           <h2 className="mb-4 text-xl sm:text-2xl font-bold tracking-tight text-gray-900">Hey!</h2>
-          <p>
+          <p className="text-sm lg:text-lg">
             Mein Name ist Vanda. Mit meinen Python-Kenntnissen und meinem Wissen über DevOps und Frontend-Technologien
             freue ich mich darauf, dich bei der Konzeption, Entwicklung und Optimierung deiner Webanwendungen zu
-            unterstützen. 
-            <br /><br />
-            Lass uns gemeinsam neue und effiziente Lösungen entwickeln, die den Zielen deines Unternehmens
-            entsprechen.
+            unterstützen.
+            <br />
+            <br />
+            Lass uns gemeinsam neue und effiziente Lösungen entwickeln, die den Zielen deines Unternehmens entsprechen.
           </p>
         </article>
       </div>
