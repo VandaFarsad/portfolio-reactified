53ce12afae8b9364f3180e0c3aac3d4ac6be7c98
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-17 23:08:25 +0100

    fetch stack from backend

--- frontend/src/api/iconData.ts
+++ frontend/src/api/iconData.ts
@@ -1,50 +1,20 @@
-export const iconDataTailwind = [
-  {
-    iconSetPrefix: "logos",
-    iconName: "python",
-    text: "Python",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[logos--python]",
-  },
-  {
-    iconSetPrefix: "skill-icons",
-    iconName: "django",
-    text: "Django",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[skill-icons--django]",
-  },
-  {
-    iconSetPrefix: "skill-icons",
-    iconName: "docker",
-    text: "Docker",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[skill-icons--docker]",
-  },
-  {
-    iconSetPrefix: "logos",
-    iconName: "kubernetes",
-    text: "Kubernetes",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[logos--kubernetes]",
-  },
-  {
-    iconSetPrefix: "logos",
-    iconName: "gitlab",
-    text: "Gitlab",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[logos--gitlab]",
-  },
-  {
-    iconSetPrefix: "skill-icons",
-    iconName: "typescript",
-    text: "TypeScript",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[skill-icons--typescript]",
-  },
-  {
-    iconSetPrefix: "devicon",
-    iconName: "nextjs",
-    text: "Next.js",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[devicon--nextjs]",
-  },
-  {
-    iconSetPrefix: "logos",
-    iconName: "react",
-    text: "React.js",
-    icon: "lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 icon-[logos--react]",
-  },
-];
+export type StackIcon = {
+  iconSetPrefix: string;
+  iconName: string;
+  text: string;
+  icon: string;
+  position: number;
+};
+
+export const fetchStackIcons = async (): Promise<StackIcon[]> => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_URL_API}/stackicons`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return (await response.json()) as Promise<StackIcon[]>;
+  } catch (error) {
+    console.error(`Failed to fetch StackIcons: ${error}`);
+    return [];
+  }
+};
--- frontend/src/components/Stack.tsx
+++ frontend/src/components/Stack.tsx
@@ -1,21 +1,45 @@
-import { iconDataTailwind } from "../api/iconData";
+import { useEffect, useState } from "react";
+import { StackIcon, fetchStackIcons } from "../api/iconData";
+
+type FetchState = "initial" | "loading" | "success" | "error";
 
 const Stack = () => {
+  const [stackIcons, setStackIcons] = useState<StackIcon[]>([]);
+  const [state, setState] = useState<FetchState>("initial");
+
+  useEffect(() => {
+    setState("loading");
+    fetchStackIcons()
+      .then((icon) => {
+        setStackIcons(icon);
+        setState("success");
+      })
+      .catch(() => {
+        setState("error");
+      });
+  }, []);
+
   return (
     <section id="stack" className="w-full p-4 sm:p-10 md:p-40 font-sans flex flex-col justify-center items-center">
-      <div className="col-md-12 mx-auto pb-10 pt-8">
-        <div className="sm:text-right pb-8 pl-4">
-          <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-pink-600">Tech Stack</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-4 sm:gap-8 px-4 pb-6">
-        {iconDataTailwind.map((data, index) => (
-          <div key={index} className="flex flex-col justify-center items-center px-4 sm:px-8">
-            <span className={data.icon}></span>
-            <p className="text-sm sm:text-base">{data.text}</p>
+      {state === "loading" && <h2>Loading stacks...</h2>}
+      {state === "error" && <h2>Error while loading stacks...</h2>}
+      {state === "success" && (
+        <>
+          <div className="col-md-12 mx-auto pb-10 pt-8">
+            <div className="sm:text-right pb-8 pl-4">
+              <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-pink-600">Tech Stack</p>
+            </div>
           </div>
-        ))}
-      </div>
+          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-4 sm:gap-8 px-4 pb-6">
+            {stackIcons.map((data, index) => (
+              <div key={index} className="flex flex-col justify-center items-center px-4 sm:px-8">
+                <span className={data.icon}></span>
+                <p className="text-sm sm:text-base">{data.text}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </section>
   );
 };
