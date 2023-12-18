711bbcb1c442cd032ddf032f10f1586a1bec96fa
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-18 10:29:55 +0100

    stretch image such that image and text have the same size

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -7,22 +7,23 @@ const About = () => {
       className="w-full p-4 sm:p-10 lg:p-40 font-sans bg-slate-500 flex flex-col justify-center items-center"
     >
       <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-pink-600">About</p>
-      <div className="w-full pt-8 grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-16 pb-6">
-        <img
-          className="rounded-2xl shadow-2xl shadow-slate-300 max-w-xs mx-auto sm:max-w-sm sm:max-h-xl lg:mr-4"
-          src={photo}
-          alt="portrait"
-        />
-        <article className="max-w-xs mx-auto sm:max-w-sm sm:max-h-xl lg:max-w-xl lg:ml-4 p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300">
+      <div className="w-full pt-8 grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-16 pb-6 items-stretch">
+        <div className="flex">
+          <img
+            className="rounded-2xl shadow-2xl shadow-slate-300 max-w-xs mx-auto sm:max-w-sm lg:mr-4 object-cover self-stretch"
+            src={photo}
+            alt="portrait"
+          />
+        </div>
+        <article className="max-w-xs mx-auto sm:max-w-sm lg:max-w-xl lg:ml-4 p-6 bg-white border border-gray-200 rounded-lg shadow-2xl shadow-slate-300 flex flex-col">
           <h2 className="mb-4 text-xl sm:text-2xl font-bold tracking-tight text-gray-900">Hey!</h2>
-          <p className="text-sm lg:text-lg">
+          <p className="text-sm lg:text-base xl:text-lg  lg:max-h-18 flex-grow">
             Ich bin Vanda und bringe Python-Kenntnisse sowie Erfahrung in DevOps und Frontend-Technologien mit. Gerne
             stehe ich zur Verfügung, um dich bei der Konzeption, Entwicklung und Optimierung deiner Webanwendungen zu
             unterstützen.
             <br />
             <br />
-            Lass uns gemeinsam innovative und wirkungsvolle Lösungen entwickeln, um die Effizienz der Arbeitsprozesse in
-            deinem Unternehmen zu erhöhen.
+            Lass uns gemeinsam innovative und wirkungsvolle Lösungen für deine Arbeitsprozesse entwickeln.
           </p>
         </article>
       </div>
