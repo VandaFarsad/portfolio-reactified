0eafc50db585d08dbfeb304f6feea7ba80f5c929
Vanda Farsad <contact@initial-commit.com>
2023-12-19 16:48:19 +0000

    fix wrong import

--- frontend/src/screens/MainScreen.tsx
+++ frontend/src/screens/MainScreen.tsx
@@ -1,8 +1,8 @@
-import { Stack } from "@mui/material";
 import About from "../components/About";
 import Experience from "../components/Experience";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Stack from "../components/Stack";
 
 const MainScreen = () => {
   return (
