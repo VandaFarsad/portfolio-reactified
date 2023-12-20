17a22686f656fdccf435afba59786b619c422529
Vanda Farsad <contact@initial-commit.com>
2023-12-20 15:02:39 +0000

    do not redirect requests on django to react

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 from drf_spectacular.views import (
@@ -7,9 +7,16 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
+django_urls = [
+    "admin",
+    "api",
+]
+# Match any URL that doesn't start with one of the above
+react_urls = r"^(?!" + "|".join(django_urls) + r")"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),
+    re_path(react_urls, TemplateView.as_view(template_name="index.html")),
     path("api/", include("api.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
